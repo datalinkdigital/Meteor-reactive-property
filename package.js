f21ea9bc2fe07eef578d620d73f6b122cfecc05a
datalinkdigital <hendra.charles@gmail.com>
2014-06-22 20:52:20 +0800

    Update package.js

--- package.js
+++ package.js
@@ -10,10 +10,3 @@ Package.on_use(function (api) {
   api.add_files(['reactive-property.js'], ['client', 'server']);
 });
 
-Package.on_test(function (api) {
-  api.use('power-queue');
-  api.use('test-helpers', 'server');
-  api.use('tinytest');
-
-  api.add_files('tests.js');
-});
